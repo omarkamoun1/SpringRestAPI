cf86e74d1f80a954dfa4497f6faaf5dc8bcc08ee
jschidiac <62688251+jschidiac@users.noreply.github.com>
2021-02-23 05:21:19 -0500

    remove warning

--- src/main/java/com/jobdiva/api/model/chatbot/ChatbotHarvestMachine.java
+++ src/main/java/com/jobdiva/api/model/chatbot/ChatbotHarvestMachine.java
@@ -1,19 +1,16 @@
 package com.jobdiva.api.model.chatbot;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @SuppressWarnings("serial")
 public class ChatbotHarvestMachine implements java.io.Serializable {
 	
 	@JsonProperty(value = "machineNo", index = 0)
-	public Long machineNo;
+	public Long		machineNo;
 	//
 	@JsonProperty(value = "isMachineInstalled", index = 1)
-	public boolean isMachineInstalled;
+	public boolean	isMachineInstalled;
 	//
 	@JsonProperty(value = "hasMachineIssue", index = 2)
-	public boolean hasMachineIssue;
-	
+	public boolean	hasMachineIssue;
 }
